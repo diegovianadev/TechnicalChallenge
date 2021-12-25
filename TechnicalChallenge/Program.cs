fb65f3c4bc7035a2a2c57ed073ad2d55b4b28031
Diego Viana <dtviana90@gmail.com>
2021-12-25 16:21:47 -0300

    Add project files.

---
+++ TechnicalChallenge/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using Divisor;
+
+namespace TechnicalChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um Numero:");
+            int enterNumber = int.Parse(Console.ReadLine());
+        }
+    }
+}
