cbe6c49c9436fdc2f9f866b58c6a122ab8614b1e
Diego Viana <dtviana90@gmail.com>
2021-12-25 18:04:03 -0300

    Added WebApi to solution.

---
+++ TechnicalChallenge.WebApi/Controllers/DivisorController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using DivisorLibrary;
+
+namespace TechnicalChallenge.WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DivisorController : ControllerBase
+    {
+        private readonly ILogger<DivisorController> _logger;
+
+        public DivisorController(ILogger<DivisorController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public DivisorParameter Get(int enterNumber)
+        {
+            var result = Divisor.NumberValidator(enterNumber);
+            return (result);
+        }
+
+    }
+}
