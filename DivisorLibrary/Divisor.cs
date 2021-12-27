6eeb079a14e3a1bd10d906c8199832e778726575
Diego Viana <dtviana90@gmail.com>
2021-12-26 21:13:48 -0300

    Refactored divisor

--- DivisorLibrary/Divisor.cs
+++ DivisorLibrary/Divisor.cs
@@ -7,9 +7,7 @@
         {
             DivisorParameter newParameter = new DivisorParameter();
 
-            newParameter.Divisors.Add(1);
-
-            for (int i = 2; i <= (parameter.EnterNumber/2); i++)
+            for (int i = 1; i <= (parameter.EnterNumber/2); i++)
             {
                 if (parameter.EnterNumber % i == 0)
                 {
